b8d2ebe71b153f60907e39bd963c22cf4526d0b3
Emily Chaplais <emilychaplais@hotmail.com>
2019-11-29 10:38:44 +0000

    Updates

---
+++ precache-manifest.35f40f538ac214af7f1b96398800fea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d974c5dc6beeea10f33f93396b835a26",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "8183fd9b6ee00a309f38",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "cb249a726e333f6ca7cb",
+    "url": "/github-finder/static/js/2.9dac1946.chunk.js"
+  },
+  {
+    "revision": "8183fd9b6ee00a309f38",
+    "url": "/github-finder/static/js/main.e00400ea.chunk.js"
+  },
+  {
+    "revision": "5f1f259c977a7c780146",
+    "url": "/github-finder/static/js/runtime-main.b6e0201c.js"
+  }
+]);
